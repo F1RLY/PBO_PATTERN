6ef1877175a5025be0b3575de10caf1d840aa370
F1RLY <firlyalamsudrajat@gmail.com>
2025-11-07 08:52:39 +0700

    !

--- SERVICE_LAYER_PATTERN/service/ProductServiceDefault.java
+++ SERVICE_LAYER_PATTERN/service/ProductServiceDefault.java
@@ -6,8 +6,24 @@ import java.util.ArrayList;
 public class ProductServiceDefault implements ProductService{
     private final ArrayList<Product> productsList = new ArrayList<>();
 
-    public addProduct(String name, long price) {
+    public ProductServiceDefault(){
+        productsList.add(new Product(1 , "Laptop Asus", 950000));
+        productsList.add(new Product(2 , "Monitor Dell", 2500000));
+    }
+
+    @Override
+    public void addProduct(String name, long price){
+        if (price <= 0) {
+            throw new IllegalArgumentException("Harga harus lebih dari 0");
+        }
+        int newId = productsList.size() + 1;
+        productsList.add(new Product(newId, name, price));
         
+    }
+
+    @Override
+    public ArrayList<Product> getAllProducts() {
+        return productsList;
     }
     
 }
