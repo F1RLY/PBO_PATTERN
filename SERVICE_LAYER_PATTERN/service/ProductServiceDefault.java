5519f67f7be4086861fbb2ca4657614ab2a51c72
F1RLY <firlyalamsudrajat@gmail.com>
2025-11-07 08:11:52 +0700

    !

---
+++ SERVICE_LAYER_PATTERN/service/ProductServiceDefault.java
@@ -0,0 +1,13 @@
+package service;
+
+import model.Product;
+import java.util.ArrayList;
+
+public class ProductServiceDefault implements ProductService{
+    private final ArrayList<Product> productsList = new ArrayList<>();
+
+    public addProduct(String name, long price) {
+        
+    }
+    
+}
